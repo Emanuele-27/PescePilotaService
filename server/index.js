17a711236a09d8ddff637e04710b33402cd0aa00
Emanuele <emanuele27e@gmail.com>
2021-12-15 20:28:28 +0100

    d

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ const cors = require("cors");
 
 var fs = require("fs");
 
-const file = "pescePilota.json";
+const file23 = "pescePilota23.json";
+const file24 = "pescePilota24.json";
 
 app.use(express.json());
 
@@ -18,24 +19,40 @@ app.use(
   })
 );
 
-app.get("/read", (req, res) => {
-  res.json(readFile());
+app.get("/read23", (req, res) => {
+  res.json(readFile23());
 });
 
-app.put("/write", (req, res) => {
-  let obj = readFile();
+app.get("/read24", (req, res) => {
+  res.json(readFile24());
+});
+
+app.put("/write23", (req, res) => {
+  let obj = readFile23();
 
   for (let i in obj.pesci) {
     if (obj.pesci[i].nome === req.body.nome) {
       obj.pesci[i] = req.body;
     }
   }
-  fs.writeFileSync(file, JSON.stringify(obj, null, 2));
+  fs.writeFileSync(file23, JSON.stringify(obj, null, 2));
   res.sendStatus(200);
 });
 
-app.delete("/delete", (req, res) => {
-  let obj = readFile();
+app.put("/write24", (req, res) => {
+  let obj = readFile24();
+
+  for (let i in obj.pesci) {
+    if (obj.pesci[i].nome === req.body.nome) {
+      obj.pesci[i] = req.body;
+    }
+  }
+  fs.writeFileSync(file24, JSON.stringify(obj, null, 2));
+  res.sendStatus(200);
+});
+
+app.delete("/delete23", (req, res) => {
+  let obj = readFile23();
 
   for (let i in obj.pesci) {
     obj.pesci[i].peso = 0;
@@ -43,7 +60,20 @@ app.delete("/delete", (req, res) => {
     obj.pesci[i].pezzature = [];
   }
 
-  fs.writeFileSync(file, JSON.stringify(obj, null, 2));
+  fs.writeFileSync(file23, JSON.stringify(obj, null, 2));
+  res.sendStatus(200);
+});
+
+app.delete("/delete24", (req, res) => {
+  let obj = readFile24();
+
+  for (let i in obj.pesci) {
+    obj.pesci[i].peso = 0;
+    obj.pesci[i].pezzi = 0;
+    obj.pesci[i].pezzature = [];
+  }
+
+  fs.writeFileSync(file24, JSON.stringify(obj, null, 2));
   res.sendStatus(200);
 });
 
@@ -51,7 +81,12 @@ app.listen(PORT, () => {
   console.log(`PescePilotaService listening on ${PORT}`);
 });
 
-function readFile() {
-  let json = fs.readFileSync(file);
+function readFile23() {
+  let json = fs.readFileSync(file23);
+  return JSON.parse(json);
+}
+
+function readFile24() {
+  let json = fs.readFileSync(file24);
   return JSON.parse(json);
 }
